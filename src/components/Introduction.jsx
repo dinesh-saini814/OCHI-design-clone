518ab7454e33deb09dbd15a2bcb1fb3b68379144
dinesh-saini814 <dm43102154@gmail.com>
2024-02-20 23:42:36 +0530

    added big text section in introduction part

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -1,7 +1,41 @@
 import React from "react";
 
 function Introduction() {
-  return <div className="w-full py-20 bg-customGreen"></div>;
+  return (
+    <div className="w-full bg-customGreen rounded-tl-3xl rounded-tr-3xl text-charcoal font-['Neue_Montreal'] ">
+      <h1 className="text-[4vw] leading-none pt-20 p-10">
+        Ochi is a strategic partner for fast-growing tech businesses that need
+        to raise funds, sell products, explain complex ideas, and hire great
+        people.
+      </h1>
+      <div className="w-full border-[1px] border-zinc-600">
+        <div className="flex justify-between p-10">
+          <div className="flex ">
+            <h2 className="text-[1.5vw] w-fit">What you can expect:</h2>
+          </div>
+          <div className="flex flex-col">
+            <p className="text-[1.5vw]">
+              We create tailored presentations to help you persuade your
+              colleagues, clients, or investors. Whether it's live or digital,
+              delivered for one or a hundred people.
+            </p>
+            <p className="text-[1.5vw]">
+              We believe the mix of strategy and design (with a bit of coffee)
+              is what makes your message clear, convincing, and captivating.
+            </p>
+          </div>
+          <div className="flex flex-col list-none">
+            <p className="text-[1.5vw]">S:</p>
+            {["Instagram", "Becance", "Facebook", "LinkedIn"].map(
+              (item, index) => (
+                <li>{item}</li>
+              )
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Introduction;
--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -5,10 +5,10 @@ function Marquee() {
   return (
     <div className="w-full pt-24 pb-5 bg-marquee rounded-tl-3xl rounded-tr-3xl">
       <div className="text border-t-[1px] border-b-[1px] flex gap-5 overflow-hidden whitespace-nowrap">
-        <h1 className="text-[26vw] leading-none font-['Test_Founders_Grotesk_X_Condensed'] -mt-16 uppercase -mb-5 font-semibold">
+        <h1 className="text-[26vw] leading-none font-['Test_Founders_Grotesk_X_Condensed'] -mt-[5vw] py-1 uppercase -mb-5 font-semibold">
           We Are Ochi
         </h1>
-        <h1 className="text-[26vw] leading-none font-['Test_Founders_Grotesk_X_Condensed'] -mt-16 uppercase -mb-5 font-semibold">
+        <h1 className="text-[26vw] leading-none font-['Test_Founders_Grotesk_X_Condensed'] -mt-[5vw] py-1 uppercase -mb-5 font-semibold">
           We Are Ochi
         </h1>
       </div>
