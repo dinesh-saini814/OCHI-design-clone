c963c1acae66e41e2e5ebc3c1fbdb6b9661033fe
dinesh-saini814 <dm43102154@gmail.com>
2024-03-07 23:57:48 +0530

    added side picture

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 function Introduction() {
   return (
-    <div className="w-full bg-customGreen rounded-tl-3xl rounded-tr-3xl text-charcoal font-['Neue_Montreal'] ">
+    <div className="w-full  bg-customGreen rounded-tl-3xl rounded-tr-3xl text-charcoal font-['Neue_Montreal'] ">
       <h1 className="text-[4vw] leading-none pt-20 p-10">
         Ochi is a strategic partner for fast-growing tech businesses that need
         to raise funds, sell products, explain complex ideas, and hire great
         people.
       </h1>
       <div className="w-full flex gap-5 border-t-[1px] border-b-[1px] border-zinc-600">
-        <div className="flex justify-between p-10 mb-24">
+        {/* <div className="flex  justify-between p-10 mb-24">
           <div className="flex ">
             <h2 className="text-[1.5vw] w-fit">What you can expect:</h2>
           </div>
@@ -32,7 +32,7 @@ function Introduction() {
               )
             )}
           </div>
-        </div>
+        </div> */}
       </div>
       <div className="w-1/2  pt-210 px-10">
         <h1 className="text-[4vw] mb-2">Our approach:</h1>
@@ -41,7 +41,7 @@ function Introduction() {
           <div className="w-2 h-2 bg-white rounded-full"></div>
         </button>
       </div>
-      <div className="w-1/2 h-[70vh] bg-black"></div>
+      <div className="w-1/2 h-[70vh] bg-red-400 rounded"></div>
     </div>
   );
 }
