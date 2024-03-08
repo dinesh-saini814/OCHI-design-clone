ce5b193610764f7d8688014deb8487c3bba98599
dinesh-saini814 <dm43102154@gmail.com>
2024-03-08 23:54:46 +0530

    added playEye part

--- src/components/PlayEyes.jsx
+++ src/components/PlayEyes.jsx
@@ -7,12 +7,16 @@ function PlayEyes() {
         <div className="absolute flex gap-10 top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]">
           <div className="w-[15vw] h-[15vw] flex items-center justify-center bg-zinc-100 rounded-full">
             <div className="w-3/5 h-3/5 flex items-center justify-center bg-zinc-900 rounded-full">
-              <div className="w-1/6 h-1/6 bg-zinc-100 rounded-full"></div>
+              <div className="line w-full h-10 rotate-6">
+                <div className="w-5 h-5 bg-zinc-100 rounded-full"></div>
+              </div>
             </div>
           </div>
           <div className="w-[15vw] h-[15vw] flex items-center justify-center bg-zinc-100 rounded-full">
             <div className="w-3/5 h-3/5 flex items-center justify-center bg-zinc-900 rounded-full">
-              <div className="w-1/6 h-1/6 bg-zinc-100 rounded-full"></div>
+              <div className="line w-full h-10 rotate-6">
+                <div className="w-5 h-5 bg-zinc-100 rounded-full"></div>
+              </div>
             </div>
           </div>
         </div>
