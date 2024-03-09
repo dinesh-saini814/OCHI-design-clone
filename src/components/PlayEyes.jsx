19b0c34e45a6d1b53ebc84440baca1abdc301cc4
dinesh-saini814 <dm43102154@gmail.com>
2024-03-09 22:15:15 +0530

    added animation in eyes

--- src/components/PlayEyes.jsx
+++ src/components/PlayEyes.jsx
@@ -1,21 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function PlayEyes() {
+  const [rotation, setRotation] = useState(0);
+
+  useEffect(() => {
+    window.addEventListener("mousemove", (e) => {
+      let mouseX = e.clientX;
+      let mouseY = e.clientY;
+
+      let deltaX = mouseX - window.innerWidth / 2;
+      let deltaY = mouseY - window.innerHeight / 2;
+
+      var angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
+      setRotation(angle - 180);
+    });
+  });
+
   return (
     <div className="w-full h-screen overflow-hidden">
       <div className="relative w-full h-full bg-cover bg-center bg-[url('https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg')]">
         <div className="absolute flex gap-10 top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]">
           <div className="w-[15vw] h-[15vw] flex items-center justify-center bg-zinc-100 rounded-full">
-            <div className="w-3/5 h-3/5 flex items-center justify-center bg-zinc-900 rounded-full">
-              <div className="line w-full h-10 rotate-6">
-                <div className="w-5 h-5 bg-zinc-100 rounded-full"></div>
+            <div className="relative w-3/5 h-3/5 bg-zinc-900 rounded-full">
+              <div
+                style={{
+                  transform: `translate(-50%, -50%) rotate(${rotation}deg)`,
+                }}
+                className="line absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-full h-5  "
+              >
+                <div className=" w-5 h-5 bg-zinc-100 rounded-full "></div>
               </div>
             </div>
           </div>
           <div className="w-[15vw] h-[15vw] flex items-center justify-center bg-zinc-100 rounded-full">
-            <div className="w-3/5 h-3/5 flex items-center justify-center bg-zinc-900 rounded-full">
-              <div className="line w-full h-10 rotate-6">
-                <div className="w-5 h-5 bg-zinc-100 rounded-full"></div>
+            <div className="relative w-3/5 h-3/5 bg-zinc-900 rounded-full">
+              <div
+                style={{
+                  transform: `translate(-50%, -50%) rotate(${rotation}deg)`,
+                }}
+                className="line absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-full h-5  "
+              >
+                <div className=" w-5 h-5 bg-zinc-100 rounded-full "></div>
               </div>
             </div>
           </div>
