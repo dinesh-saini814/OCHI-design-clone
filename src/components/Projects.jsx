caeb62be5223e2502289a9bfc97d59fe2c8fa113
dinesh-saini814 <dm43102154@gmail.com>
2024-03-09 22:34:15 +0530

    added project section

---
+++ src/components/Projects.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Projects() {
+  return (
+    <div className="w-full py-10 mt-14 border-b-[1px] border-zinc-500 border-opacity-5 text-charcoal bg-background">
+      <div className="w-full px-12">
+        <h1 className="text-[3.25rem] font-['Neue_Montreal'] ">
+          Featured projects
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Projects;
