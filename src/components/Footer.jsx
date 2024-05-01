38018f0e2bd4659a65d30838e957eecf0582d3d3
dinesh-saini814 <dm43102154@gmail.com>
2024-05-01 23:22:01 +0530

    started footer part

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <div className="w-full h-screen ">
-      <div className="w-1/2 font-['Test_Founders_Grotesk_X_Condensed'] text-black">
+      <div className="w-1/2 font-['Test_Founders_Grotesk_X_Condensed'] text-charcoal text-9xl p-12 leading-none">
         <h1 className="">EYE-</h1>
         <h1 className="">OPENING</h1>
       </div>
